8677e9555e84b48293888a772d26a2c6cef45474
renato <renatoathaydes@hotmail.com>
2014-03-22 14:11:21 +0100

    Simplified Evaluators

--- src/main/groovy/com/athaydes/ml/utils/Evaluators.groovy
+++ src/main/groovy/com/athaydes/ml/utils/Evaluators.groovy
@@ -9,29 +9,18 @@ import groovy.transform.Memoized
  */
 class Evaluators {
 
-	final Closure numberEvaluator = { Program p1, Program p2 ->
-		def p1Val = p1.eval()
-		def p2Val = p2.eval()
-		if ( !p1Val ) return p2Val ? 1 : 0
-		if ( !p2Val ) return p1Val ? -1 : 0
-		def v1 = Math.abs( p1.specification.out.first() - p1Val )
-		def v2 = Math.abs( p2.specification.out.first() - p2Val )
-		v1 != v2 ? toInt( v1 - v2 ) : p1.code.size() - p2.code.size()
+	final Closure numberEvaluator = { Program p ->
+		def p1Val = p.eval()
+		if ( !p1Val ) return Integer.MAX_VALUE
+		def diff = Math.abs( p.specification.out.first() - p1Val )
+		p.code.size() + diff
 	}
 
-	final Closure stringEvaluator = { Program p1, Program p2 ->
-		String p1Val = p1.eval()
-		String p2Val = p2.eval()
-		if ( !p1Val ) return p2Val ? 1 : 0
-		if ( !p2Val ) return p1Val ? -1 : 0
-		def v1 = stringDistance p1.specification.out.first(), p1Val
-		def v2 = stringDistance p2.specification.out.first(), p2Val
-		v1 != v2 ? v1 - v2 : p1.code.size() - p2.code.size()
-	}
-
-	int toInt( value ) {
-		if ( value > 0 ) Math.ceil( value )
-		else Math.floor( value )
+	final Closure stringEvaluator = { Program p ->
+		String p1Val = p.eval()
+		if ( !p1Val ) return Integer.MAX_VALUE
+		def distance = stringDistance p.specification.out.first(), p1Val
+		p.code.size() + distance
 	}
 
 	// see https://en.wikipedia.org/wiki/Levenshtein_distance
